cb312cb4f3d09a68a9d7df908e35576e0a91d2ad
José Armando Melchor <jose.melchor@iteso.mx>
2025-10-17 09:50:28 -0600

    Word & main

--- libraries.py
+++ libraries.py
@@ -7,6 +7,7 @@ import ta
 import numpy as np
 import pandas as pd
 import scipy as sp
+import yfinance as yf
 
 # --- Third-party libraries: Visualization ---
 import seaborn as sns
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from libraries import *
 
+data = yf.download("AZO", start="2010-10-10", end="2025-10-10")["Close"]
+
+#train, test, validation = dataset_split(data, 0.6, 0.2, 0.2)
+
